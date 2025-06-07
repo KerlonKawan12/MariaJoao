4a0bf4d914ba9a823fa14c504646f446c99d1ec5
KerlonKawan12 <primosflip@gmail.com>
2025-06-07 19:47:59 -0300

    Aincks

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ setInterval(() => {
     location.reload();
   }, 30000);
 
-  // Remove a tela de introdução após 2 segundos
+  // Remove a tela de introdução com fade após 2,5 segundos
   window.addEventListener('DOMContentLoaded', () => {
     const intro = document.getElementById('intro');
 
     setTimeout(() => {
       intro.classList.add('fade-out');
 
-      // Remove do DOM após a transição (1 segundo)
+      // Remove completamente do DOM após o fade-out (1s)
       setTimeout(() => {
         intro.remove();
       }, 1000);
-    }, 2000); // Espera 2 segundos antes de sumir
+
+    }, 2500); // Aguarda 2,5 segundos antes de sumir
   });
